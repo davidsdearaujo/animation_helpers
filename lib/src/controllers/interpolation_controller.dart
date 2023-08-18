dbed45344c3a095fb253f30c27e76552346ee0ae
David Araujo <david@flutterando.com.br>
2023-08-18 11:20:48 -0300

    dart version bump

--- lib/src/controllers/interpolation_controller.dart
+++ lib/src/controllers/interpolation_controller.dart
@@ -1,24 +1,34 @@
-import 'package:flutter/foundation.dart';
+import 'dart:math';
 
 class InterpolationController {
   final double begin;
   final double end;
 
   const InterpolationController({
-    @required this.begin,
-    @required this.end,
-  })  : assert(begin != null),
-        assert(end != null);
-        
+    required this.begin,
+    required this.end,
+  });
+
+  ///It applies the Linear Interpolation formula [[click to read more about it]](https://www.cuemath.com/linear-interpolation-formula/)
+  ///It can be used to create animations [example](https://dartpad.dev/?id=f2c14f4da20f3b72eb82e0d136dd9343):
   ///F(x) = y0 + ((y1 - y0) / (x1 - x0)) * (x - x0);
-  ///y1 = 
   double linear(
     double x, {
-    @required double begin,
-    @required double end,
+    required double begin,
+    required double end,
   }) {
-    assert(begin != null);
-    assert(end != null);
     return begin + ((end - begin) / (this.end - this.begin)) * (x - this.begin);
+  }
+
+  /// The same as `linear()` but it applies `clamp()` function in the result
+  double linearClamped(
+    double x, {
+    required double begin,
+    required double end,
+  }) {
+    return linear(x, begin: begin, end: end).clamp(
+      min(begin, end),
+      max(begin, end),
+    );
   }
 }
--- lib/src/utils/stable_font_size.dart
+++ lib/src/utils/stable_font_size.dart
@@ -3,10 +3,7 @@ import 'package:flutter/material.dart';
 class StableFontSize extends StatelessWidget {
   final Widget child;
   final double factor;
-
-  const StableFontSize({Key key, @required this.child, this.factor = 500})
-      : assert(child != null),
-        super(key: key);
+  const StableFontSize({super.key, required this.child, this.factor = 500});
 
   @override
   Widget build(BuildContext context) {
